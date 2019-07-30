28ab3d9baf519c83f62f314771910426a4064c0b
cchowdhary <cchowdhary@internal.tavisca.com>
2019-07-30 14:14:17 +0530

    Server setup

---
+++ src/main/java/com/tavisca/chat/ChatWebSocket.java
@@ -0,0 +1,40 @@
+package com.tavisca.chat;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+
+@ServerEndpoint("/endpoint")
+public class ChatWebSocket {
+    private ChatHelper chatHelper;
+
+    ChatWebSocket() {
+        chatHelper = new ChatHelper();
+    }
+
+    @OnOpen
+    public void onOpen(Session session) {
+        System.out.println("Open Connenction: " + session.getId());
+        ChatHelper.sessionHashMap.put(session.getId(), session);
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+        System.out.println("Close Connection: " + session.getId());
+        ChatHelper.sessionHashMap.remove(session.getId());
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        System.out.println("Received message: from" + session.getId() + "Message is " + message);
+        chatHelper.sendMessage(message, session.getId());
+        try {
+            session.getBasicRemote().sendText(message.toUpperCase());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
